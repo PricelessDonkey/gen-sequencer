440bc6672fc23e3ad635f24756f6c4526d8f95cc
PricelessDonkey <62314090+PricelessDonkey@users.noreply.github.com>
2024-12-02 03:24:15 +0000

    initial layout

--- gen-sequencer/src/app/app.component.ts
+++ gen-sequencer/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gen-sequencer';
+  activeSection: string = 'A'; // Tracks the active section
+
+  toggleSection(section: string) {
+    this.activeSection = section;
+  }
 }
