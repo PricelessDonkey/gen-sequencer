a9d3ce2fd9f80038a9e8e878a203ac7903c37c86
PricelessDonkey <62314090+PricelessDonkey@users.noreply.github.com>
2024-12-06 03:51:11 +0000

    start of sequencer

--- gen-sequencer/src/app/app.component.ts
+++ gen-sequencer/src/app/app.component.ts
@@ -1,16 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
+type Step = {
+  id: number,
+  selected?: boolean,
+  index?: number,
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gen-sequencer';
   activeSection: string = 'A'; // Tracks the active section
+  seq1Randomness = 0;
+  seq1CurrentStep: number | undefined;
+  seq1: Step[] = [
+    { id: 0 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+  ];
+  seq2Randomness = 0;
+  seq2CurrentStep: number | undefined;
+  seq2: Step[] = [
+    { id: 0 },
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+  ];
 
   toggleSection(section: string) {
     this.activeSection = section;
